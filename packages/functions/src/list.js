fb6ff4e4ecfe4f0d5f8f2e78d8efd97b2967018a
Eli Craig <elidcraig@gmail.com>
2023-04-07 16:44:05 -0400

    Securing the API

--- packages/functions/src/list.js
+++ packages/functions/src/list.js
@@ -2,11 +2,11 @@ import { Table } from "sst/node/table";
 import handler from "@notes/core/handler";
 import dynamoDb from "@notes/core/dynamodb";
 
-export const main = handler(async () => {
+export const main = handler(async (event) => {
   const params = {
     TableName: Table.Notes.tableName,
     KeyConditionExpression: "userId = :userId",
-    ExpressionAttributeValues: { ":userId": "123" },
+    ExpressionAttributeValues: { ":userId": event.requestContext.authorizer.iam.cognitoIdentity.identityId },
   };
 
   const result = await dynamoDb.query(params);
